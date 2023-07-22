80f72fd2857f83e9a8084932b79586344f436547
lotuxsoo <tndus5383@gmail.com>
2023-07-22 11:55:12 +0900

    7/22

--- Client/week4/src/App.js
+++ Client/week4/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import TestPage from "./pages/TestPage";
+import TestPage1 from "./pages/TestPage1";
+import TestPage2 from "./pages/TestPage2";
+import TestPage3 from "./pages/TestPage3";
 import ResultPage from "./pages/ResultPage";
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/test" element={<TestPage />} />
+        <Route path="/test1" element={<TestPage1 />} />
+        <Route path="/test2" element={<TestPage2 />} />
+        <Route path="/test3" element={<TestPage3 />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
     </Router>
--- Client/week4/src/pages/MainPage.js
+++ Client/week4/src/pages/MainPage.js
@@ -3,15 +3,27 @@ import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const goToNext = () => {
-    navigate("/test"); // '/next-path'를 원하는 경로로 변경하세요.
+  const goTo1 = () => {
+    navigate("/test1"); // '/next-path'를 원하는 경로로 변경하세요.
+  };
+  const goTo2 = () => {
+    navigate("/test2"); // '/next-path'를 원하는 경로로 변경하세요.
+  };
+  const goTo3 = () => {
+    navigate("/test3"); // '/next-path'를 원하는 경로로 변경하세요.
   };
 
   return (
     <>
       <div>main</div>
-      <button style={{ marginTop: "30px" }} type="button" onClick={goToNext}>
-        GO
+      <button style={{ marginTop: "30px" }} type="button" onClick={goTo1}>
+        test1
+      </button>
+      <button style={{ marginTop: "30px" }} type="button" onClick={goTo2}>
+        test2
+      </button>
+      <button style={{ marginTop: "30px" }} type="button" onClick={goTo3}>
+        test3
       </button>
     </>
   );
