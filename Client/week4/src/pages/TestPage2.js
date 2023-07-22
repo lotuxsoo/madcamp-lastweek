18f27b255c51c0e805944277f7898ad154ad5610
lotuxsoo <https://github.com/lotuxsoo>
2023-07-22 12:39:49 +0900

    12:39

--- Client/week4/src/pages/TestPage2.js
+++ Client/week4/src/pages/TestPage2.js
@@ -1,18 +1,43 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import "../css/form.css";
+import "../css/reset.css";
+import "../css/result.css";
+import "../css/style.css";
 
-const TestPage2 = () => {
-  const navigate = useNavigate();
-  const goToNext = () => {
-    navigate("/result"); // '/next-path'를 원하는 경로로 변경하세요.
-  };
+function TestPage2() {
   return (
-    <>
-      <div>test2</div>
-      <button style={{ marginTop: "30px" }} type="button" onClick={goToNext}>
-        GO
-      </button>
-    </>
+    <section id="survey">
+      <div className="wrapper">
+        <form id="form" action="/submit/" method="post">
+          <input
+            type="hidden"
+            name="csrfmiddlewaretoken"
+            value="a3Np13S61gdwUtBoimms9BhSFLK6InNFKnOKXRFBzLZGouatjuH3kBk1e64bEXE1"
+          />
+          <div className="test">
+            <div className="question_container">
+              <h3 className="number">1/10</h3>
+              <h3 className="question">당신이 가장 재밌었던 수업은?</h3>
+            </div>
+            <div className="answer">
+              <div>
+                <input
+                  type="radio"
+                  name="question-1"
+                  id="choice-1"
+                  value="5"
+                  required
+                />
+                <label htmlFor="choice-1">
+                  1. 게임 개발(유니티, 언리얼, Pygame)
+                </label>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   );
-};
+}
+
 export default TestPage2;
