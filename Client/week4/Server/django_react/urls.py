070369bf174b9da70327d2edefd11a9e3d399df1
lotuxsoo <tndus5383@gmail.com>
2023-07-26 21:13:22 +0900

    0913

--- Client/week4/Server/django_react/urls.py
+++ Client/week4/Server/django_react/urls.py
@@ -2,19 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     # path('', views.index),
-    # path('form/', views.form),
-    # path('result/', views.result),
     path('test1/', views.test1),
     path('test2/', views.test2),
-    # path("hello/", views.helloAPI),
-    # path('developers/', views.developer_list),
-    # path('questions/', views.question_list),
-    # path('choices/', views.choice_list),
     path('test4/', views.test4),
-#     path("hello/", views.helloAPI),
-#     path('developers/', views.developer_list),
-#     path('questions/', views.question_list),
-#     path('choices/', views.choice_list),
 ]
